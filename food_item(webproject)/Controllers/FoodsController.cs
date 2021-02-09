e559171688502065d0d9ea88c452aebf48a03598
Maria Waheed <63168444+MariaWaheed@users.noreply.github.com>
2021-02-09 20:54:36 +0500

    Add files via upload

---
+++ food_item(webproject)/Controllers/FoodsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using food_item_webproject_.Models;
+
+namespace food_item_webproject_.Controllers
+{
+    public class FoodsController : ApiController
+    {
+        private FoodMenuEntities7 db = new FoodMenuEntities7();
+
+        // GET: api/Foods
+        public IQueryable<Food> GetFoods()
+        {
+            return db.Foods;
+        }
+
+        // GET: api/Foods/5
+        [ResponseType(typeof(Food))]
+        public IHttpActionResult GetFood(int id)
+        {
+            Food food = db.Foods.Find(id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(food);
+        }
+
+        // PUT: api/Foods/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutFood(int id, Food food)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != food.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(food).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FoodExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Foods
+        [ResponseType(typeof(Food))]
+        public IHttpActionResult PostFood(Food food)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Foods.Add(food);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = food.ID }, food);
+        }
+
+        // DELETE: api/Foods/5
+        [ResponseType(typeof(Food))]
+        public IHttpActionResult DeleteFood(int id)
+        {
+            Food food = db.Foods.Find(id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            db.Foods.Remove(food);
+            db.SaveChanges();
+
+            return Ok(food);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool FoodExists(int id)
+        {
+            return db.Foods.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ food_item(webproject)/Models/FoodItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+
+namespace food_item_webproject_.Models
+{
+    public class FoodItem
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Type { get; set; }
+        public int Price { get; set; }
+    }
+}
